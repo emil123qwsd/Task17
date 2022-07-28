e45c267ad24e14cdae9d9d5fa7fee5792ad0d76c
эмиль ахметханов <emil.ah@yandex.ru>
2022-07-28 22:51:31 +0300

    Добавьте файлы проекта.

---
+++ App1/Bank.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Bank<T>
+    {
+        public T Num { get; set; }
+        public string Name { get; set; }
+        public int Summa { get; set; }
+
+        public string GetInfoC()
+        {
+            Console.WriteLine("Тип счета: текущиий");//Текущий счет пример:123456
+            return $"Номер счета: {Num}, ФИО: {Name}, Баланс: {Summa}";
+        }
+        public string GetInfoS()
+        {
+            Console.WriteLine("Тип счета: сберегательный");//Сберегательный счет пример:ТН1234АП
+            return $"Номер счета: {Num}, ФИО: {Name}, Баланс: {Summa}";
+        }
+        public void Input()
+        {
+            Console.WriteLine("Введите номер");
+            Num = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите ФИО");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите баланс");
+            Summa = Convert.ToInt32(Console.ReadLine());
+        }
+
+        
+    }
+}
---
+++ App1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank<int> bank1 = new Bank<int>();
+            bank1.Input();
+            Console.WriteLine(bank1.GetInfoC());
+
+            Bank<string> bank2 = new Bank<string>();
+            bank2.Input();
+            Console.WriteLine(bank2.GetInfoS());
+
+            Console.ReadKey();
+
+        }
+    }
+}
